0d45163f4d18ac01cea4ea21dea2badd06f2d1f0
Angel Samuel Espinoza <145504767+Hl-0@users.noreply.github.com>
2025-01-06 11:57:50 -0800

    finish the first video
    The first video is complete but the second is half way done

---
+++ Graph&WayPointsEspinozaP3/Assets/WayPoiny.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoiny : MonoBehaviour
+{
+    public GameObject[] wayPoints;
+    int currentWP = 0;
+
+    public float speed = 10.0f;
+    public float rotspeed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(this.transform.position, wayPoints[currentWP].transform.position) < 3) 
+            currentWP++;
+
+        if (currentWP >= wayPoints.Length)
+            currentWP = 0;
+
+        //this.transform.LookAt(wayPoints[currentWP].transform.position);
+
+
+        Quaternion LookatWP = Quaternion.LookRotation(wayPoints[currentWP].transform.position - this.transform.position);
+
+        this.transform.rotation = Quaternion.Slerp(transform.rotation, LookatWP, rotspeed * Time.deltaTime);    
+
+        this.transform.Translate(0,0, speed * Time.deltaTime);
+    }
+}
